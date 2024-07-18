4e3bed5b8fab122c05426d111f4360c6d3b5378d
Chiranth-45 <150377345+Chiranth-45@users.noreply.github.com>
2024-07-18 21:07:24 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// for backend nodejs this file is used
-
-module.exports=123;
-
-// if u  do like this this will not improve your git bash knowledge
-// u want to learn another things also so please donot practice only one thing explore the topic in same thing deeply
